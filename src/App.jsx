4f0c16c867252c7b01b2ab47304c5043587f498f
Patrik <trikteixeira@gmail.com>
2023-11-15 21:40:07 -0300

    estados e eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-import MeuComponente from "./components/MeuComponente"
+import MeuContador from "./components/MeuContador"
 
-function App() {
+export default function App() {
 
   return (
     <div>
-      <h1>Olá Mundo, React!</h1>
-      <MeuComponente />
-      <MeuBotao conteudo='me clique' />
-
-      <MeuBotao conteudo='depois aqui'/>
-
-      <MeuBotao conteudo='e por fim aqui' idade={2} />
+      <MeuContador />
     </div>
   )
 }
-
-function MeuBotao(props) {
-
-  return (
-    <button>{props.conteudo}</button>
-  )
-}
-
-export default App
