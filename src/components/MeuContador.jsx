e64b488c46a1019de88f205ff734e42443b50351
Patrik <trikteixeira@gmail.com>
2023-11-15 21:56:30 -0300

    Renderização condicional

--- src/components/MeuContador.jsx
+++ src/components/MeuContador.jsx
@@ -7,12 +7,25 @@ export default function MeuContador() {
     function aumentar() {
         setContador(contador + 1)
     }
+    function diminuir() {
+        setContador(contador - 1)
+    }
+
+    if (contador > 5) {
+        return (
+            <div>
+                <h1>Valor muito alto!</h1>     
+                <button onClick={aumentar}>aumentar</button>   
+                <button onClick={diminuir}>diminuir</button>
+            </div>
+        )
+    }
 
     return (
         <div>
-            <h1>Meu Contador</h1>
-            <h3>{contador}</h3>
-            <button onClick={aumentar}>aumentar</button>
+            <h1>Meu Contador: {contador}</h1>
+            <button onClick={aumentar}>aumentar</button>   
+            <button onClick={diminuir}>diminuir</button>
         </div>
     )
 }
