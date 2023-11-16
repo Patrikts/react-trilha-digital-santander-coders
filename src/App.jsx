5cb387a0e87a9a27476b34d065eed483d40700a2
Patrik <trikteixeira@gmail.com>
2023-11-15 21:23:18 -0300

    Componentes e props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import MeuComponente from "./components/MeuComponente"
+
 function App() {
 
   return (
-    <h1>Olá Mundo, React!</h1>
+    <div>
+      <h1>Olá Mundo, React!</h1>
+      <MeuComponente />
+      <MeuBotao conteudo='me clique' />
+
+      <MeuBotao conteudo='depois aqui'/>
+
+      <MeuBotao conteudo='e por fim aqui' idade={2} />
+    </div>
+  )
+}
+
+function MeuBotao(props) {
+
+  return (
+    <button>{props.conteudo}</button>
   )
 }
 
---
+++ src/components/MeuComponente.jsx
@@ -0,0 +1,5 @@
+export default function MeuComponente()  {
+    return (
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae quos labore libero facere placeat error ut aliquam, voluptatem doloremque iusto cum, autem illum quibusdam saepe! Ea, quasi, maiores veritatis nisi eum iure porro numquam impedit voluptas est possimus dolores officia corporis quibusdam necessitatibus sapiente molestias doloribus consectetur minus ullam excepturi?</p>
+    )
+  }
