f7851782b3059d8c38540fdf6047f6c27a7cd98d
Patrik <trikteixeira@gmail.com>
2023-11-15 22:14:44 -0300

    Listas e Chaves

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import MeuContador from "./components/MeuContador"
+import MinhaLista from "./components/MinhaLista";
 
 export default function App() {
 
   return (
     <div>
-      <MeuContador />
+      <h1>Listas no React</h1>
+      <MinhaLista />
     </div>
   )
 }
